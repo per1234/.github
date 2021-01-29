a13886883d13548f0d4d02bfcc1554ed10953ef8
per1234 <accounts@perglass.com>
2021-01-29 07:46:20 -0800

    f(dependabot

--- workflow-templates/dependabot-copies/sync.sh
+++ workflow-templates/dependabot-copies/sync.sh
@@ -4,4 +4,5 @@
 readonly SCRIPT_PATH="$(cd -P "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
 readonly WORKFLOW_COPIES_PATH="${SCRIPT_PATH}/.github/workflows"
 rm "${WORKFLOW_COPIES_PATH}/"*
-cp "${SCRIPT_PATH}/../"*.{yml,yaml} "$WORKFLOW_COPIES_PATH"
+cp "${SCRIPT_PATH}/../"*.yml "$WORKFLOW_COPIES_PATH" || :
+cp "${SCRIPT_PATH}/../"*.yaml "$WORKFLOW_COPIES_PATH" || :
