9a06b940be478bd799d81d23cb779996568ef885
per1234 <accounts@perglass.com>
2021-02-14 13:54:20 -0800

    Add template workflow to check general file formatting
    On every push, pull request, and periodically, check whether the repository's files are formatted according to
    .editorconfig.

--- workflow-templates/etc/sync.sh
+++ workflow-templates/etc/sync.sh
@@ -16,6 +16,7 @@ cp \
 
 # Sync workflows.
 cp \
+  "${WORKFLOW_TEMPLATES_PATH}/check-general-formatting.yml" \
   "${WORKFLOW_TEMPLATES_PATH}/check-markdown.yml" \
   "${WORKFLOW_TEMPLATES_PATH}/check-prettier-formatting.yml" \
   "${WORKFLOW_TEMPLATES_PATH}/check-shell.yml" \
